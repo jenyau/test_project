5f712d38396e76391c43db1bdd01499b6170081b
jenyau <jekharitonov@gmail.com>
2020-10-09 22:06:19 +0300

    validate for limit 10 appointments complete

--- app/models/appointment.rb
+++ app/models/appointment.rb
@@ -1,5 +1,13 @@
 class Appointment < ApplicationRecord
   belongs_to :user
   belongs_to :doctor
+  validate :limit_appointment
 
+
+
+  def limit_appointment
+    if doctor.appointments.where(status: true).length > 2
+      errors.add(:doctor_id, "has more than 10 open appointments")
+    end
+  end
 end
